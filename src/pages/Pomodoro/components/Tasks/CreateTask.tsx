669a8dd81fdd7ff07cdc32b62adee21c39eb97c6
Matthew Akuhata <matthewakuhata@gmail.com>
2022-12-11 20:15:18 +1100

    Add create task component

--- src/pages/Pomodoro/components/Tasks/CreateTask.tsx
+++ src/pages/Pomodoro/components/Tasks/CreateTask.tsx
@@ -1,23 +1,60 @@
-import React from "react";
-import TaskItem from "./TaskItem";
-const task = {
-  title: "Cook DinnerCook DinnerCook Dinner",
-  description: "roast lamb with veggies",
-  pomodoros: 4,
-  isComplete: true,
-};
+import React, { createRef, useRef, useState } from "react";
+import { Button, Input } from "../../../../shared/components/Form";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import "./CreateTask.scss";
 
 const CreateTask = () => {
+  const [hasNote, setHasNote] = useState(false);
+  const pomoRef = createRef<HTMLInputElement>();
+
+  const updatePomos = (val: number) => {
+    if (!pomoRef || !pomoRef.current) return;
+
+    const newValue = parseInt(pomoRef.current.value, 10) + val;
+    if (newValue < 1) return;
+
+    pomoRef.current.value = newValue.toString();
+  };
+
   return (
-    <div className="task-item">
-      <div className="task-item__heading">
-        <h2>Title</h2>
-        <input className="title" type="text" />
+    <div className="create-task task-item">
+      <Input
+        placeholder="What are you working on?"
+        className="create-task__title"
+        type="text"
+      />
+      <label>Est. Pomodoro's</label>
+      <div className="create-task__pomo">
+        <Input
+          ref={pomoRef}
+          defaultValue={1}
+          min={1}
+          className="create-task__pomo__input"
+          type="number"
+        />
+        <Button
+          onClick={() => updatePomos(1)}
+          className="create-task__pomo__button"
+        >
+          <KeyboardArrowUpIcon />
+        </Button>
+        <Button
+          onClick={() => updatePomos(-1)}
+          className="create-task__pomo__button"
+        >
+          <KeyboardArrowDownIcon />
+        </Button>
       </div>
-      <label>Description</label>
-      <input className="description" type="text" />
-      <label>Pomodoros:</label>
-      <input className="pomodoros" type="text" />
+      <Button
+        className="create-task__note"
+        displaySize="xs"
+        displayType="outline"
+        onClick={() => setHasNote((prev) => !prev)}
+      >
+        {hasNote ? "Remove Note" : "+ Add Note"}
+      </Button>
+      {hasNote && <Input className="create-task__desc" type="text" />}
     </div>
   );
 };

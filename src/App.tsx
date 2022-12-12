b968f1ca5534b7a5241141f49801c18431e617c3
Matthew Akuhata <matthewakuhata@gmail.com>
2022-12-12 14:09:07 +1100

    Add wrapper for components

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,35 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./shared/components/Navigation/Header";
 import { Pomodoro } from "./pages/Pomodoro";
 import { TimersProvider } from "./pages/Pomodoro/contexts/timers";
+import React from "react";
 
 // TODO: Maybe add CSSTransition package.
 function App() {
   return (
-    <TimersProvider>
-      <div className="App">
-        <Header />
-        <BrowserRouter>
-          <Routes>
-            {/* Add AppWrap for elements usecontext inside route */}
-            <Route path="/" element={<Pomodoro />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </TimersProvider>
+    <div className="App">
+      {/* <Header /> */}
+      <BrowserRouter>
+        <Routes>
+          {/* Add AppWrap for elements usecontext inside route */}
+          <Route
+            path="/"
+            element={
+              <TimersProvider>{WrapHeader(<Pomodoro />)}</TimersProvider>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
+
+const WrapHeader = (components: React.ReactNode) => {
+  return (
+    <>
+      <Header />
+      {components}
+    </>
+  );
+};
 
 export default App;
